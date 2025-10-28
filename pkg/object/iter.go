35b39c4bda49d85dbf14f6ab169c5d5ab68007e5
Morlay <morlay.null@gmail.com>
2025-10-28 14:20:53 +0800

    fix(object): converted object should replace

--- pkg/object/iter.go
+++ pkg/object/iter.go
@@ -3,9 +3,6 @@ package object
 import (
 	"iter"
 	"sync/atomic"
-
-	"github.com/octohelm/x/anyjson"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 type Process = func(o Object) (Object, error)
@@ -52,6 +49,25 @@ func (w *objectIter) Object(m any) iter.Seq[Object] {
 	}
 }
 
+func (w *objectIter) normalizeAndProcess(o Object) (Object, error) {
+	typed, err := Convert(o)
+	if err != nil {
+		return nil, err
+	}
+
+	if typed != o {
+		o = typed
+	}
+
+	for i := range w.progress {
+		o, err = w.progress[i](o)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return o, nil
+}
+
 func (w *objectIter) iter(v any) iter.Seq[Object] {
 	switch x := v.(type) {
 	case Object:
@@ -61,66 +77,73 @@ func (w *objectIter) iter(v any) iter.Seq[Object] {
 			}
 		}
 	case map[string]any:
-		return w.iterObj(x)
-	case []any:
-		return w.iterList(x)
-	}
-	return func(yield func(Object) bool) {
-	}
-}
-
-func (w *objectIter) iterList(list []any) iter.Seq[Object] {
-	return func(yield func(Object) bool) {
-		for _, value := range list {
-			for o := range w.iter(value) {
-				if !yield(o) {
-				}
-			}
-		}
-	}
-}
-
-func (w *objectIter) iterObj(obj anyjson.Obj) iter.Seq[Object] {
-	return func(yield func(Object) bool) {
-		if isKubernetesManifest(obj) {
-			o, err := FromRuntimeObject(&unstructured.Unstructured{Object: obj})
-			if err != nil {
-				w.Done(err)
-				return
-			}
-
-			typed, err := Convert(o)
-			if err != nil {
-				w.Done(err)
-				return
-			}
-
-			if typed != o {
-				o = typed
-			}
-
-			for i := range w.progress {
-				o, err = w.progress[i](o)
+		obj, ok := Is(x)
+		if ok {
+			return func(yield func(Object) bool) {
+				normalized, err := w.normalizeAndProcess(obj)
 				if err != nil {
 					w.Done(err)
 					return
 				}
-			}
-
-			if !yield(o) {
-				return
-			}
-
-			return
-		}
-
-		// walk child
-		for _, value := range obj {
-			for o := range w.iter(value) {
-				if !yield(o) {
+				if !yield(normalized) {
 					return
 				}
 			}
 		}
+
+		return func(yield func(Object) bool) {
+			// walk child
+			for k, value := range x {
+				o, ok := Is(value)
+				if ok {
+					normalized, err := w.normalizeAndProcess(o)
+					if err != nil {
+						w.Done(err)
+						return
+					}
+
+					if !yield(normalized) {
+						return
+					}
+
+					x[k] = normalized
+					continue
+				}
+
+				for o := range w.iter(value) {
+					if !yield(o) {
+						return
+					}
+				}
+			}
+		}
+	case []any:
+		return func(yield func(Object) bool) {
+			for i, value := range x {
+				o, ok := Is(value)
+				if ok {
+					normalized, err := w.normalizeAndProcess(o)
+					if err != nil {
+						w.Done(err)
+						return
+					}
+
+					if !yield(normalized) {
+						return
+					}
+
+					x[i] = normalized
+					continue
+				}
+
+				for o := range w.iter(value) {
+					if !yield(o) {
+						return
+					}
+				}
+			}
+		}
+	}
+	return func(yield func(Object) bool) {
 	}
 }
